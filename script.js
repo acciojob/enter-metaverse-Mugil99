ecb2642c69174654b0b8569ca33a6995410bc188
Mugil99 <124775550+Mugil99@users.noreply.github.com>
2023-04-28 22:02:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const p = document.getElementById("status")
+const btn = document.getElementById("enterBtn")
+const body = document.querySelector("body")
+function changeText(){
+    const h1 = document.createElement("h1")
+    h1.innerText = "Entered Metaverse"
+    p.innerHTML = ""
+	p.appendChild(h1)
+}
+btn.addEventListener("click", changeText)
